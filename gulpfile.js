496944419d4b6c3a4ee181d56d94494b1a9306ca
Mohammad Eslami <email@mohammadeslami.com>
2016-02-01 08:21:34 -0500

    enhanced web

--- gulpfile.js
+++ gulpfile.js
@@ -7,34 +7,35 @@ var gulp = require('gulp'),
   merge = require('merge2');
 
 gulp.task('less', function () {
-  gulp.src('./public/css/*.less')
+  gulp.src('./css/*.less')
     .pipe(plumber())
     .pipe(less())
-    .pipe(gulp.dest('./public/css'))
+    .pipe(gulp.dest('./css'))
     .pipe(livereload());
 });
 
 var tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('scripts', function() {
-var tsResult = gulp.src('public/app/**/*.ts')
+var tsResult = gulp.src('app/**/*.ts')
 .pipe(ts(tsProject));
  
 return merge([ // Merge the two output streams, so this task is finished when the IO of both operations are done. 
-  tsResult.dts.pipe(gulp.dest('public/bin/definitions')),
-  tsResult.js.pipe(gulp.dest('public/bin/app'))
+  tsResult.dts.pipe(gulp.dest('bin/definitions')),
+  tsResult.js.pipe(gulp.dest('bin/app'))
+  .pipe(livereload());
   ]);
 });
 
 gulp.task('watch', ['less', 'scripts'], function() {
-  gulp.watch('./public/css/*.less', ['less']);
-  gulp.watch('./public/app/**/*.ts', ['scripts']);
+  gulp.watch('./css/*.less', ['less']);
+  gulp.watch('./app/**/*.ts', ['scripts']);
 });
 
 gulp.task('develop', function () {
   livereload.listen();
   nodemon({
-    script: 'server/bin/www',
+    script: '',
     ext: 'js handlebars',
     stdout: false
   }).on('readable', function () {
