fddacd312546a4ff2afee0aa9a632f41e62dea91
Mohammad Eslami <email@mohammadeslami.com>
2016-02-07 12:36:28 -0500

    fixed home issues

--- app/shared/topnav.component.ts
+++ app/shared/topnav.component.ts
@@ -1,9 +1,31 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
+
+declare var $: any;
 
 @Component({
     selector: 'top-nav',
     templateUrl: 'shared/topnav.component.html'
 })
-export class TopNavComponent { 
+export class TopNavComponent implements OnInit {
 	public loginTitle = 'Welcome Mohammad!';
+
+	ngOnInit() {
+		$('.custom-menu a[href^="#"], .intro-scroller .inner-link').on('click', function(e) {
+			e.preventDefault();
+
+			var target = this.hash;
+			var $target = $(target);
+
+			$('html, body').stop().animate({
+				'scrollTop': $target.offset().top
+			}, 900, 'swing', function() {
+				window.location.hash = target;
+			});
+		});
+
+		$(".nav a").on("click", function() {
+			$(".nav").find(".active").removeClass("active");
+			$(this).parent().addClass("active");
+		});
+	}
 }
